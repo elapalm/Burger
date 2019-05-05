e87d42d9d289828d785ce7cc3eba0a6775695719
emma lapalm <lillypadcrafts@gmail.com>
2019-05-04 19:40:31 -0700

    testing controller file. working on ORM next

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -1,6 +1,41 @@
 Inside the burgers_controller.js file, import the following:
+// Run npm for Express
+var express = require('express');
 
+//burger.js
+var burger = require('../models/burger.js');
 
+// the main route
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
 
-Express
-burger.js
+      res.render('index', hbsObject);
+    });
+  });
+  // the route to burger database
+  router.post('/burgers', function(req, res) {
+    burger.insertOne([
+      'burger_name'
+    ], [
+      req.body.burger_name
+    ], function(data) {
+      res.redirect('/');
+    });
+  });
+
+  //
+  router.put('/burgers/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+  
+    burger.updateOne({
+      devoured: true
+    }, condition, function(data) {
+      res.redirect('/');
+    });
+  });
+
+  
+  module.exports = router;
